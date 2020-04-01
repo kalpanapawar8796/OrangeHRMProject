8deff73b8fcf90ffe10f944be0f4456fa30702a4
kalpanapawar8796 <kalpana.pawar94@gmail.com>
2020-04-01 17:50:44 +0530

    My 3rd commit

--- src/test/java/Test/TestHello.java
+++ src/test/java/Test/TestHello.java
@@ -7,5 +7,6 @@ public class TestHello {
 	@Test
 	public void myTest(){
 		System.out.println("Testing......");
+		System.out.println("git config \n•	git init \n•	git clone \n•	git add\n•	git commit\n•	git diff\n•	git reset\n•	git status\n•	git rm\n•	git log\n•	git show\n•	git tag\n•	git branchn\n•	git checkout\n•	git merge\n•	git remote\n•	git push\n•	git pull\n•	git stash");
 	}
 }
